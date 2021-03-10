af4c2f2b911d9dcb96ee3192a31286377fc4cfb3
nikel <nikesushko@gmail.com>
2021-03-11 00:00:15 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+development:
+
+1. npm install
+2. gulp dev
+
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,6 @@ module.exports = {
       }
     ]
   },
-  mode: 'development'
-  // mode: 'production'
+  // mode: 'development'
+  mode: 'production'
 }
